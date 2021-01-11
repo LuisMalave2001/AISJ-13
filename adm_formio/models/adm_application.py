3b9bcb5ac0335f142d5a4cae1175f624e982348b
rainierking <rainierking@gitlab.com>
2021-01-11 13:20:59 +0800

    [IMP] adm_formio, formio_extends: add form smart button in inquiry and application, show student name in formio form portal and backend list view [ADD] formio_reporting

--- adm_formio/models/adm_application.py
+++ adm_formio/models/adm_application.py
@@ -8,3 +8,15 @@ class AdmApplication(models.Model):
 
     formio_sent_to_email = fields.Char(string="Formio sent to email")
     formio_reference_form_id = fields.Many2one("formio.form")
+    form_count = fields.Integer(stirng="Form Count",
+        compute="_compute_form_count")
+
+    def _compute_form_count(self):
+        for application in self:
+            application.form_count = self.env["formio.form"].search_count([("reference","=","adm.application,%s" % application.id)])
+    
+    def action_open_forms(self):
+        self.ensure_one()
+        action = self.env.ref("formio.action_formio_form").read()[0]
+        action["domain"] = [("reference","=","adm.application,%s" % self.id)]
+        return action
